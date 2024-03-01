f5e929d399b50bf8882c70136d0944817f1bd180
Reshula <reyhana328@gmail.com>
2024-03-01 13:23:20 -0500

    added unique key to MyList

--- src/MyList.js
+++ src/MyList.js
@@ -8,7 +8,7 @@ function MyList({addMeal, mealPlans, deleteDay, selectedDay, setSelectedDay}){
             </div>
             <div>
                 {mealPlans.map(({id, title, mealForADay}) =>(
-                    <div  className={`meal ${id===selectedDay ? "selected" : "default"}`} 
+                    <div key={id} className={`meal ${id===selectedDay ? "selected" : "default"}`} 
                     onClick={() =>setSelectedDay(id)}>
                         <p className="field">{title}</p>
                         <p className="field">{mealForADay.substring(0,60)}</p>
